1f7381b2ac9e867de3bb2bb13f00a29f438ac388
kevin kim <kevinkim1991@gmail.com>
2015-10-24 20:09:19 -0400

    live socket running

--- app/socket.js
+++ app/socket.js
@@ -67,34 +67,9 @@ server.listen(port);
 /* Above code working fine now socket.io working fine */
 //using socket io with express web server
 //var io = require('socket.io').listen(app);
-/*
-//Including the namespace we still have to access classes
-var mongo = require("mongodb");
-
-//Choose our host and port
-var dbHost = "127.0.0.1";
-//Get port from Connection (listening information) 
-var dbPort = mongo.Connection.DEFAULT_PORT;
-
-//Database variable which will store our connection details
-//mongo is connection Db is the class and node-js-intro is name of database
-//Then we need to pass the server to connect to then we will use mongo namespace and class called Server
-var db = new mongo.Db("node-js-intro",new mongo.Server(dbHost,dbPort,{}) );
-*/
 
 //Variable to store table name
 var tweetCollection;
-
-/*
-db.open(function(error){
-    console.log("We are connected to : " + host + ":" + port);
-
-    //Selecting a table 
-    db.collection("tweet",function(error,collection){
-        tweetCollection = collection;
-    });
-});
-*/
 
 db.on('error', function(err){
     console.log("mongoose connection error: " + err );
@@ -119,11 +94,6 @@ function getTweets(callback){
 
 /* Code that connect with twitter and streams data into database */
 
-//Getting tweets at runtime
-//Refering to table name
-var tweetCollection;
-
-
 //Defining access tokens, Get this by creating new twitter application
 var T = new Twit({
     consumer_key:           secrets.twitter.consumerKey,
@@ -133,7 +103,7 @@ var T = new Twit({
 })
 
 //Creating a request variable and using stream function 
-var request = T.stream('statuses/filter', { track: 'bieber' })
+var request = T.stream('statuses/filter', { track: 'fixitkw' })
 
 //Printing tweets related to bieber on a screen 
 request.on('tweet', function (tweet) {
@@ -141,6 +111,7 @@ request.on('tweet', function (tweet) {
     //emit is to send data to client,We supplied tweet(json) as js object
     //Sockets will serialise tweet which is deserialised at client(template.html)
     io.sockets.emit("tweet",tweet);
+    /*
     tweetCollection.insert(tweet,function(error){
         if(error){
             console.log(error);
@@ -148,6 +119,7 @@ request.on('tweet', function (tweet) {
             console.log("Inserted into database");
         }
     });
+    */
     console.log(tweet.text);
     console.log("\n");
 })
